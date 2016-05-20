12db48a1d4554800b31620eeaf1bf9fdac5b0ffe
OJdoesIt <otisj16@yahoo.com>
2016-05-20 08:46:46 -0700

    Test by OJ

---
+++ plugin_sa/game_sa/CCarCtrl.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "plbase/PluginBase.h"
+#include "game_sa\CVehicle.h"
+
+
+class PLUGIN_API CCarCtrl
+{
+public:
+	static void GetCarToParkAtCoords(CVehicle* veh, CVector* coord);
+	static void GetCarToGoToCoordsRacing(CVehicle* veh, CVector* coord, int drivingStyle, bool bSpeedLimit20);
+	static void GetCarToGoToCoords(CVehicle* veh, CVector* coord, int drivingStyle, bool bSpeedLimit20);
+	static void TellCarToRamOtherCar(CVehicle* veh, CVehicle* targetveh);
+	static void TellCarToBlockOtherCar(CVehicle* veh, CVehicle* targetveh);
+	static void TellOccupantsToLeaveCar(CVehicle* veh);
+	static bool CreatePoliceChase(CVehicle* veh, int a1, CNodeAddress unk);
+	static void JoinCarWithRoadSystem(CVehicle* veh);
+	static void MakeWayForCarWithSiren(CVehicle* veh);
+	static void PossiblyFireHSMissile(CVehicle* veh, CEntity* targetEntity);
+	static void SteerAIBoatWithPhysicsCirclingPlayer(CVehicle* veh, float* a1, float* a2, float* a3, bool* a4);
+	static CVehicle* CreateOneEmergencyServicesVehicle(int model, CVector driveToCoord);
+};
