941ec1d41a0f10d778bc80510c7054cb09702e52
DK22Pac <dk22_@mail.ru>
2016-05-22 00:38:17 +0300

    CTaskManager finished

--- plugin_sa/game_sa/CTaskManager.h
+++ plugin_sa/game_sa/CTaskManager.h
@@ -3,14 +3,32 @@
 #include "plbase/PluginBase.h"
 #include "CTask.h"
 
-#pragma pack(push, 4)
-class PLUGIN_API CTaskManager
-{
+class PLUGIN_API CTaskManager {
 public:
-	CTask *m_aPrimaryTasks[5];
-	CTask *m_aSecondaryTasks[6];
-	class CPed *m_pPed;
+    CTask *m_aPrimaryTasks[5];
+    CTask *m_aSecondaryTasks[6];
+    class CPed *m_pPed;
+    
+    CTaskManager(CPed* ped);
+    ~CTaskManager();
+    CTask* GetActiveTask();
+    CTask* FindActiveTaskByType(int taskType);
+    CTask* FindTaskByType(int taskIndex, int taskType);
+    CTask* GetTaskSecondary(int taskIndex);
+    bool HasTaskSecondary(CTask const* task);
+    void Flush();
+    void FlushImmediately();
+    void SetNextSubTask(CTask* task);
+    CTask* GetSimplestTask(CTask* task);
+    void StopTimers(CEvent const* _event);
+    CTask* GetSimplestActiveTask();
+    CTask* GetSimplestTask(int taskIndex);
+    void AddSubTasks(CTask* task);
+    void ParentsControlChildren(CTask* task);
+    void SetTask(CTask* task, int taskIndex, bool arg2);
+    void SetTaskSecondary(CTask* task, int taskIndex);
+    void ClearTaskEventResponse();
+    void ManageTasks();
 };
-#pragma pack(pop)
 
 VALIDATE_SIZE(CTaskManager, 0x30);
