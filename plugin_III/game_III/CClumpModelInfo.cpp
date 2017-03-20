256b0262903a69c6bef154dd733334f6d834fc96
kenkingGitHub <kenking@rambler.ru>
2017-03-20 15:25:26 +0600

    III::CClumpModelInfo

---
+++ plugin_III/game_III/CClumpModelInfo.cpp
@@ -0,0 +1,77 @@
+/*
+Plugin-SDK (Grand Theft Auto) source file
+Authors: GTA Community. See more here
+https://github.com/DK22Pac/plugin-sdk
+Do not delete this comment block. Respect others' work!
+*/
+#include "CClumpModelInfo.h"
+
+// Converted from void CClumpModelInfo::DeleteRwObject(void) 0x4F8800
+void CClumpModelInfo::DeleteRwObject() {
+    plugin::CallVirtualMethod<2, CClumpModelInfo *>(this);
+}
+
+// Converted from void CClumpModelInfo::CreateInstance(void) 0x4F8920
+void CClumpModelInfo::CreateInstance() {
+    plugin::CallVirtualMethod<3, CClumpModelInfo *>(this);
+}
+
+// Converted from void CClumpModelInfo::CreateInstance(RwMatrixTag *) 0x4F88A0
+void CClumpModelInfo::CreateInstance(RwMatrixTag* arg0) {
+    plugin::CallVirtualMethod<4, CClumpModelInfo *, RwMatrixTag*>(this, arg0);
+}
+
+// Converted from void CClumpModelInfo::GetRwObject(void) 0x50C1C0
+void CClumpModelInfo::GetRwObject() {
+    plugin::CallVirtualMethod<5, CClumpModelInfo *>(this);
+}
+
+// Converted from void CClumpModelInfo::SetClump(RpClump *clump) 0x4F8830
+void CClumpModelInfo::SetClump(RpClump* clump) {
+    plugin::CallVirtualMethod<6, CClumpModelInfo *, RpClump*>(this, clump);
+}
+
+// Converted from thiscall void CClumpModelInfo::CClumpModelInfo(void) 0x50C040 
+CClumpModelInfo::CClumpModelInfo() {
+    plugin::CallMethod<0x50C040, CClumpModelInfo *>(this);
+}
+
+// Converted from cdecl void CClumpModelInfo::FillFrameArray(RpClump *clump,RwFrame **frames) 0x4F8B90
+void CClumpModelInfo::FillFrameArray(RpClump* clump, RwFrame** frames) {
+    plugin::Call<0x4F8B90, RpClump*, RwFrame**>(clump, frames);
+}
+
+// Converted from cdecl void CClumpModelInfo::FillFrameArrayCB(RwFrame *frame,void *searchData) 0x4F8B20
+void CClumpModelInfo::FillFrameArrayCB(RwFrame* frame, void* searchData) {
+    plugin::Call<0x4F8B20, RwFrame*, void*>(frame, searchData);
+}
+
+// Converted from cdecl RwFrame* CClumpModelInfo::FindFrameFromIdCB(RwFrame *frame,void *searchData) 0x4F8AD0
+RwFrame* CClumpModelInfo::FindFrameFromIdCB(RwFrame* frame, void* searchData) {
+    return plugin::CallAndReturn<RwFrame*, 0x4F8AD0, RwFrame*, void*>(frame, searchData);
+}
+
+// Converted from cdecl RwFrame* CClumpModelInfo::FindFrameFromNameCB(RwFrame *frame,void *searchData) 0x4F8960
+RwFrame* CClumpModelInfo::FindFrameFromNameCB(RwFrame* frame, void* searchData) {
+    return plugin::CallAndReturn<RwFrame*, 0x4F8960, RwFrame*, void*>(frame, searchData);
+}
+
+// Converted from cdecl RwFrame* CClumpModelInfo::FindFrameFromNameWithoutIdCB(RwFrame *frame,void *searchData) 0x4F8A10
+RwFrame* CClumpModelInfo::FindFrameFromNameWithoutIdCB(RwFrame* frame, void* searchData) {
+    return plugin::CallAndReturn<RwFrame*, 0x4F8A10, RwFrame*, void*>(frame, searchData);
+}
+
+// Converted from cdecl RwFrame* CClumpModelInfo::GetFrameFromId(RpClump *clump,int id) 0x4F8B50
+RwFrame* CClumpModelInfo::GetFrameFromId(RpClump* clump, int id) {
+    return plugin::CallAndReturn<RwFrame*, 0x4F8B50, RpClump*, int>(clump, id);
+}
+
+// Converted from cdecl void CClumpModelInfo::SetAtomicRendererCB(RpAtomic *atomic,void *renderFunc) 0x4F8940
+void CClumpModelInfo::SetAtomicRendererCB(RpAtomic* atomic, void* renderFunc) {
+    plugin::Call<0x4F8940, RpAtomic*, void*>(atomic, renderFunc);
+}
+
+// Converted from thiscall void CClumpModelInfo::SetFrameIds(RwObjectNameIdAssocation *data) 0x4F8BB0
+void CClumpModelInfo::SetFrameIds(RwObjectNameIdAssocation* data) {
+    plugin::CallMethod<0x4F8BB0, CClumpModelInfo *, RwObjectNameIdAssocation*>(this, data);
+}
---
+++ plugin_III/game_III/CClumpModelInfo.h
@@ -0,0 +1,57 @@
+/*
+Plugin-SDK (Grand Theft Auto) header file
+Authors: GTA Community. See more here
+https://github.com/DK22Pac/plugin-sdk
+Do not delete this comment block. Respect others' work!
+*/
+#pragma once
+
+#include "plbase/PluginBase_III.h"
+#include "CBaseModelInfo.h"
+#include "RenderWare.h"
+#include "RwObjectNameIdAssocation.h"
+
+struct FrameSearchData {
+    char const *name;
+    RwFrame *result;
+};
+
+class CClumpModelInfo : public CBaseModelInfo {
+public:
+    RpClump *m_pClump;
+    
+    //vtable
+
+    void DeleteRwObject();
+    void CreateInstance();
+    void CreateInstance(RwMatrixTag* arg0);
+    void GetRwObject();
+    void SetClump(RpClump* clump);
+
+    //funcs
+    CClumpModelInfo(plugin::dummy_func_t) {}
+
+    CClumpModelInfo();
+    static void FillFrameArray(RpClump* clump, RwFrame** frames);
+    static void FillFrameArrayCB(RwFrame* frame, void* searchData);
+    static RwFrame* FindFrameFromIdCB(RwFrame* frame, void* searchData);
+    static RwFrame* FindFrameFromNameCB(RwFrame* frame, void* searchData);
+    static RwFrame* FindFrameFromNameWithoutIdCB(RwFrame* frame, void* searchData);
+    static RwFrame* GetFrameFromId(RpClump* clump, int id);
+    static void SetAtomicRendererCB(RpAtomic* atomic, void* renderFunc);
+    void SetFrameIds(RwObjectNameIdAssocation* data);
+
+    static inline RwFrame *GetFrameFromName(RpClump *clump, char const *name) {
+        FrameSearchData searchData;
+        searchData.name = name;
+        searchData.result = nullptr;
+        RwFrameForAllChildren(reinterpret_cast<RwFrame *>(clump->object.parent), FindFrameFromNameCB, &searchData);
+        return searchData.result;
+    }
+
+protected:
+    CClumpModelInfo(const CClumpModelInfo &) {};
+    CClumpModelInfo &operator=(const CClumpModelInfo &) {};
+};
+
+VALIDATE_SIZE(CClumpModelInfo, 0x34);
