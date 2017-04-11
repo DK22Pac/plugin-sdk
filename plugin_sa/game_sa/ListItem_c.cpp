ebdc1e7b3dcfc5eae42342f5d4af0fafea8348c1
nick7 <nick7@nick7.com>
2017-04-11 04:20:20 +0300

    List_c / ListItem_c calling code updated

--- plugin_sa/game_sa/ListItem_c.cpp
+++ plugin_sa/game_sa/ListItem_c.cpp
@@ -3,11 +3,11 @@
 // US-1.00 @ 0x004A8DB0
 // EU-1.00 @ 0x004A8DB0
 ListItem_c::ListItem_c(void) {
-	reinterpret_cast<void(__thiscall *)(ListItem_c *)>(0x004A8DB0)(this);
+	plugin::CallMethod<0x004A8DB0>(this);
 }
 
 // US-1.00 @ 0x004A8DC0
 // EU-1.00 @ 0x0049EA70
 ListItem_c::~ListItem_c(void) {
-	reinterpret_cast<void(__thiscall *)(ListItem_c *)>(0x004A8DC0)(this);
+	plugin::CallMethod<0x004A8DC0>(this);
 }
--- plugin_sa/game_sa/List_c.cpp
+++ plugin_sa/game_sa/List_c.cpp
@@ -4,27 +4,27 @@
 // US-1.00 @ 0x004A8DD0
 // EU-1.00 @ 0x004A8DD0
 List_c::List_c(void) {
-	reinterpret_cast<void (__thiscall *)(List_c *)>(0x004A8DD0)(this);
+	plugin::CallMethod<0x004A8DD0>(this);
 }
 
 // US-1.00 @ 0x004A8DE0
 // EU-1.00 @ 0x004A8DE0
 List_c::~List_c(void) {
-	reinterpret_cast<void (__thiscall *)(List_c *)>(0x004A8DE0)(this);
+	plugin::CallMethod<0x004A8DE0>(this);
 }
 
 // US-1.00 @ 0x004A8DF0
 // EU-1.00 @ 0x004A8DF0
 void List_c::AddItem(ListItem_c * pItem) {
 	assert(pItem);
-	return reinterpret_cast<void (__thiscall *)(List_c *, ListItem_c *)>(0x004A8DF0)(this, pItem);
+	return plugin::CallMethod<0x004A8DF0, List_c *, ListItem_c *>(this, pItem);
 }
 
 // US-1.00 @ 0x004A8E30
 // EU-1.00 @ 0x004A8E30
 void List_c::RemoveItem(ListItem_c * pItem) {
 	assert(pItem);
-	return reinterpret_cast<void (__thiscall *)(List_c *, ListItem_c *)>(0x004A8E30)(this, pItem);
+	return plugin::CallMethod<0x004A8E30, List_c *, ListItem_c *>(this, pItem);
 }
 
 ListItem_c * List_c::GetHead(void) {
@@ -33,7 +33,7 @@ ListItem_c * List_c::GetHead(void) {
 
 // EU-1.00 @ 0x004A8E70
 ListItem_c * List_c::RemoveHead(void) {
-	return reinterpret_cast<ListItem_c * (__thiscall *)(List_c *)>(0x004A8E70)(this);
+	return plugin::CallMethodAndReturn<ListItem_c *, 0x004A8E70, List_c *>(this);
 }
 
 ListItem_c * List_c::GetNext(ListItem_c * pItem) {
@@ -45,11 +45,11 @@ ListItem_c * List_c::GetNext(ListItem_c * pItem) {
 // EU-1.00 @ 0x004A9000
 ListItem_c * List_c::GetPrev(ListItem_c * pItem) {
 	assert(pItem);
-	return reinterpret_cast<ListItem_c * (__thiscall *)(List_c *, ListItem_c *)>(0x004A9000)(this, pItem);
+	return plugin::CallMethodAndReturn<ListItem_c *, 0x004A9000, List_c *, ListItem_c *>(this, pItem);
 }
 
 // US-1.00 @ 0x004A9010
 // EU-1.00 @ 0x004A9010
 ListItem_c * List_c::GetItemOffset(Bool bFromHead, Int32 iOffset) {
-	return reinterpret_cast<ListItem_c * (__thiscall *)(List_c *, Bool, Int32)>(0x004A9010)(this, bFromHead, iOffset);
+	return plugin::CallMethodAndReturn<ListItem_c *, 0x004A9010, List_c *, Bool, Int32>(this, bFromHead, iOffset);
 }
