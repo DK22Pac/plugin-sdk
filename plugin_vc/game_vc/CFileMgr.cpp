119e4a2dbe5a12b831d5a818ab2041ad30bfe294
DK22Pac <gdamdk@gmail.com>
2017-06-24 17:39:39 +0300

    VC/ CFileMgr

---
+++ plugin_vc/game_vc/CFileMgr.cpp
@@ -0,0 +1,75 @@
+/*
+    Plugin-SDK (Grand Theft Auto) source file
+    Authors: GTA Community. See more here
+    https://github.com/DK22Pac/plugin-sdk
+    Do not delete this comment block. Respect others' work!
+*/
+#include "CFileMgr.h"
+
+char *CFileMgr::ms_dirName = (char *)0x7F9C18;
+char *CFileMgr::ms_rootDirName = (char *)0x68BD50;
+
+// Converted from cdecl int CFileMgr::GetErrorReadWrite(int fileHandle) 0x48DE90
+int CFileMgr::GetErrorReadWrite(int fileHandle) {
+    return plugin::CallAndReturn<int, 0x48DE90, int>(fileHandle);
+}
+
+// Converted from cdecl int CFileMgr::CloseFile(int fileHandle) 0x48DEA0
+int CFileMgr::CloseFile(int fileHandle) {
+    return plugin::CallAndReturn<int, 0x48DEA0, int>(fileHandle);
+}
+
+// Converted from cdecl bool CFileMgr::ReadLine(int fileHandle,char *buffer,int maxSize) 0x48DEB0
+bool CFileMgr::ReadLine(int fileHandle, char* buffer, int maxSize) {
+    return plugin::CallAndReturn<bool, 0x48DEB0, int, char*, int>(fileHandle, buffer, maxSize);
+}
+
+// Converted from cdecl bool CFileMgr::Seek(int fileHandle,int offset,int origin) 0x48DEE0
+bool CFileMgr::Seek(int fileHandle, int offset, int origin) {
+    return plugin::CallAndReturn<bool, 0x48DEE0, int, int, int>(fileHandle, offset, origin);
+}
+
+// Converted from cdecl int CFileMgr::Write(int fileHandle,char *buffer,int size) 0x48DF30
+int CFileMgr::Write(int fileHandle, char* buffer, int size) {
+    return plugin::CallAndReturn<int, 0x48DF30, int, char*, int>(fileHandle, buffer, size);
+}
+
+// Converted from cdecl int CFileMgr::Read(int fileHandle,char *buffer,int size) 0x48DF50
+int CFileMgr::Read(int fileHandle, char* buffer, int size) {
+    return plugin::CallAndReturn<int, 0x48DF50, int, char*, int>(fileHandle, buffer, size);
+}
+
+// Converted from cdecl int CFileMgr::OpenFileForWriting(char const*filepath) 0x48DF70
+int CFileMgr::OpenFileForWriting(char const* filepath) {
+    return plugin::CallAndReturn<int, 0x48DF70, char const*>(filepath);
+}
+
+// Converted from cdecl int CFileMgr::OpenFile(char const*filepath,char const*mode) 0x48DF90
+int CFileMgr::OpenFile(char const* filepath, char const* mode) {
+    return plugin::CallAndReturn<int, 0x48DF90, char const*, char const*>(filepath, mode);
+}
+
+// Converted from cdecl int CFileMgr::LoadFile(char const*filepath,uchar *buffer,int size,char const*mode) 0x48DFB0
+int CFileMgr::LoadFile(char const* filepath, unsigned char* buffer, int size, char const* mode) {
+    return plugin::CallAndReturn<int, 0x48DFB0, char const*, unsigned char*, int, char const*>(filepath, buffer, size, mode);
+}
+
+// Converted from cdecl void CFileMgr::SetDirMyDocuments(void) 0x48E020
+void CFileMgr::SetDirMyDocuments() {
+    plugin::Call<0x48E020>();
+}
+
+// Converted from cdecl void CFileMgr::SetDir(char const*dir) 0x48E030
+void CFileMgr::SetDir(char const* dir) {
+    plugin::Call<0x48E030, char const*>(dir);
+}
+
+// Converted from cdecl void CFileMgr::ChangeDir(char const*dir) 0x48E090
+void CFileMgr::ChangeDir(char const* dir) {
+    plugin::Call<0x48E090, char const*>(dir);
+}
+
+// Converted from cdecl void CFileMgr::Initialise(void) 0x48E100
+void CFileMgr::Initialise() {
+    plugin::Call<0x48E100>();
+}
---
+++ plugin_vc/game_vc/CFileMgr.h
@@ -0,0 +1,30 @@
+/*
+    Plugin-SDK (Grand Theft Auto) header file
+    Authors: GTA Community. See more here
+    https://github.com/DK22Pac/plugin-sdk
+    Do not delete this comment block. Respect others' work!
+*/
+#pragma once
+#include "plbase\PluginBase_VC.h"
+
+#define FILESTREAM int
+
+class PLUGIN_API CFileMgr {
+public:
+    static char *ms_dirName; // static char ms_dirName[128]
+    static char *ms_rootDirName; // static char ms_rootDirName[128]
+
+    static int GetErrorReadWrite(FILESTREAM fileHandle);
+    static int CloseFile(FILESTREAM fileHandle);
+    static bool ReadLine(FILESTREAM fileHandle, char* buffer, int maxSize);
+    static bool Seek(FILESTREAM fileHandle, int offset, int origin);
+    static int Write(FILESTREAM fileHandle, char* buffer, int size);
+    static int Read(FILESTREAM fileHandle, char* buffer, int size);
+    static FILESTREAM OpenFileForWriting(char const* filepath);
+    static FILESTREAM OpenFile(char const* filepath, char const* mode);
+    static FILESTREAM LoadFile(char const* filepath, unsigned char* buffer, int size, char const* mode);
+    static void SetDirMyDocuments();
+    static void SetDir(char const* dir);
+    static void ChangeDir(char const* dir);
+    static void Initialise();
+};
