6ae6e8c9234d9e132e6169f65d28696e65b4e787
Crspy <crspo2020@outlook.com>
2017-09-24 22:04:38 +0200

    updated CTheZones functions
    added a lot of missing functions and fixed a naming mistake at  "CTheZones::Update"  was actually "CTheZones::SetCurrentZoneAsUnlocked"

--- plugin_sa/game_sa/CTheZones.h
+++ plugin_sa/game_sa/CTheZones.h
@@ -37,33 +37,63 @@ public:
 	static CZone* MapZoneArray;
 
 	// Functions
-
+	
+	static void InitZonesPopulationSettings();
+	
+	static void ResetZonesRevealed();
+		
+	static void AssignZoneInfoForThisZone(short index);
+	
+	static bool ZoneIsEntirelyContainedWithinOtherZone(CZone* pZone1, CZone* pZone2);
+	
+	static bool GetCurrentZoneLockedOrUnlocked(float posx, float posy);
+	
+	// Returns true if point lies within zone
+	static bool PointLiesWithinZone(CVector const* pPoint, CZone * pZone);
+	
+	// Returns eLevelName from position
+	static eLevelName GetLevelFromPosition(CVector const* pPoint);
+	
+	// Returns pointer to zone by a point
+	static CZone* FindZoneForPoint(const CVector& point, bool FindOnlyZonesType0);
+	
+	static CTheZones* GetZoneInfo(CVector *point, CZone **outzone);
+	
+	static void FillZonesWithGangColours(bool DisableRadarGangColors);
+	
+	// Returns pointer to zone by index
+	static CZone* GetInfoZone(short index);
+	
+	// Returns pointer to zone by index
+	static CZone* GetMapZone(short index);
+	
+	static long double Calc2DDistanceBetween2Zones(CZone *Zone1, CZone *Zone2);
+	
+	// Initializes CTheZones
+	static void Init();
+	
+	// Unlock the current zone
+	static void SetCurrentZoneAsUnlocked();
+	
 	// Creates a zone
 	static void CreateZone(const char *name, eZoneType type, float posX1, float posY1, float posZ1, float posX2, float posY2, float posZ2, eLevelName island, const char *GXT_key);
 
-	// Returns eLevelName from position
-	static eLevelName GetLevelFromPosition(CVector const* pPoint);
-
+	// Returns 1 if point within the specified zonename otherwise return 0
+	static bool PointLiesWithinZoneNamed(CVector* point, int zonename_part1 ,int zonename_part2,eZoneType type);
+	
 	// Returns pointer to zone by index
-	static CZone* GetInfoZone(short index);
-
-	// Returns pointer to zone by index
-	static CZone* GetMapZone(short index);
-
-	// Returns pointer to zone by index
-	static short FindZoneByLabelAndReturnIndex(const char* name, int unk2);
-
-	// Returns pointer to zone by a point
-	static CZone* FindZoneForPoint(const CVector& point, bool FindOnlyZonesType0);
-
-	// Initializes CTheZones
-	static void Init();
-
-	// Returns true if point lies within zone
-	static bool PointLiesWithinZone(CVector const* pPoint, CZone * pZone);
-
+	static short FindZoneByLabelAndReturnIndex(const char* name, eZoneType type);
+	
+	static void SetZoneRadarColours(short index, char flag, unsigned char red, unsigned char green, unsigned char blue);
+	
 	// Updates CTheZones info
 	static void Update();
+	
+	// Save CTheZones info
+	static void Save();
+
+	// Load CTheZones info
+	static void Load();
 
 };
 #pragma pack(pop)
