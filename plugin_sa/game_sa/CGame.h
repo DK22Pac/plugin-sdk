f1addf756ad251700a039923f5394c52e3468f18
Crspy <crspo2020@outlook.com>
2017-09-27 21:37:51 +0200

    Updating CGame Class

--- plugin_sa/game_sa/CGame.h
+++ plugin_sa/game_sa/CGame.h
@@ -12,7 +12,33 @@ class PLUGIN_API CGame
 {
 public:
 	// Current number of area
-	static __int32& currArea;
+	static int& currArea;
 
+	static int &currLevel;
+	static bool &bMissionPackGame;
+
+	static char *aDatFile;  // char aDatFile[28]
+
+	//funcs
 	static void TidyUpMemory(bool arg1, bool arg2);
+
+	static void InitialiseOnceBeforeRW();
+	static void ShutdownRenderWare();
+	static void FinalShutdown();
+	static void Initialise(char const* filename);
+	static void ReInitGameObjectVariables();
+	static void Process();
+	static bool CanSeeOutSideFromCurrArea();
+	static bool CanSeeWaterFromCurrArea();
+	static void ShutDownForRestart();
+	static void InitialiseWhenRestarting();
+	static void DrasticTidyUpMemory(bool arg0);
+	static void Shutdown();
+	static bool InitialiseEssentialsAfterRW();
+	static void Init2();
+	static void Init3();
+	static bool InitialiseRenderWare();
+	static bool Init1(char const* filename);
+	static bool InitialiseCoreDataAfterRW();
+	static void InitAfterLostFocus();
 };
