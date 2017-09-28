6c6285e9416f308cde63732572a0c2f62346c2d4
Crspy <crspo2020@outlook.com>
2017-09-28 02:50:06 +0200

    adding CPedIntelligence Funcs

--- plugin_sa/game_sa/CPedIntelligence.h
+++ plugin_sa/game_sa/CPedIntelligence.h
@@ -15,6 +15,14 @@
 #include "CEventScanner.h"
 #include "CTaskSimpleUseGun.h"
 #include "CTaskSimpleJetpack.h"
+#include "CTaskSimpleFight.h"
+#include "CTaskSimpleHoldEntity.h"
+#include "CTaskSimpleThrowProjectile.h"
+#include "CTaskSimpleSwim.h"
+#include "CTaskSimpleDuck.h"
+#include "CTaskSimpleClimb.h"
+#include "CTaskSimpleJetpack.h"
+#include "CTaskSimpleInAir.h"
 
 class PLUGIN_API CPedIntelligence {
 public:
@@ -50,8 +58,59 @@ public:
     char gap_27C[12];
     class CEntity *m_apInterestingEntities[3];
     
-    CTaskSimpleJetPack *GetJetpackTask();
-    CTaskSimpleUseGun *GetUseGunTask();
+	void SetPedDecisionMakerType(int newtype);
+	void SetPedDecisionMakerTypeInGroup(int newtype);
+	void RestorePedDecisionMakerType();
+	void SetHearingRange(float range);
+	void SetSeeingRange(float range);
+	bool IsInHearingRange(CVector const& posn);
+	bool IsInSeeingRange(CVector const& posn);
+	bool FindRespectedFriendInInformRange();
+	bool IsRespondingToEvent(int event);
+	void AddTaskPhysResponse(CTask* task, bool arg2);
+	void AddTaskEventResponseTemp(CTask* task, bool arg2);
+	void AddTaskEventResponseNonTemp(CTask* task, bool arg2);
+	void AddTaskPrimaryMaybeInGroup(CTask* task, bool arg2);
+	CTask* FindTaskByType(int type);
+	CTaskSimpleFight* GetTaskFighting();
+	CTaskSimpleUseGun* GetTaskUseGun();
+	CTaskSimpleThrowProjectile* GetTaskThrow();
+	CTaskSimpleHoldEntity* GetTaskHold(bool arg1);
+	CTaskSimpleSwim* GetTaskSwim();
+	CTaskSimpleDuck* GetTaskDuck(bool arg1);
+	CTaskSimpleJetPack* GetTaskJetPack();
+	CTaskSimpleInAir* GetTaskInAir();
+	CTaskSimpleClimb* GetTaskClimb();
+	bool GetUsingParachute();
+	void SetTaskDuckSecondary(unsigned short arg1);
+	void ClearTaskDuckSecondary();
+	void ClearTasks(bool arg1, bool arg2);
+	void FlushImmediately(bool arg1);
+	C2dEffect* GetEffectInUse();
+	void SetEffectInUse(C2dEffect* arg1);
+	void ProcessAfterProcCol();
+	void ProcessAfterPreRender();
+	void ProcessEventHandler();
+	bool IsFriendlyWith(CPed const& ped);
+	bool IsThreatenedBy(CPed const& ped);
+	bool Respects(CPed const& ped);
+	bool IsInACarOrEnteringOne();
+	static bool AreFriends(CPed const& ped1, CPed const& ped2);
+	bool IsPedGoingSomewhereOnFoot();
+	int* GetMoveStateFromGoToTask();
+	void FlushIntelligence();
+	bool TestForStealthKill(CPed* pPed, bool arg2);
+	void RecordEventForScript(int EventID, int EventPriority);
+	bool HasInterestingEntites();
+	bool IsInterestingEntity(CEntity* pEntity);
+	void LookAtInterestingEntities();
+	void RemoveAllInterestingEntities();
+	bool IsPedGoingForCarDoor();
+	float CanSeeEntityWithLights(CEntity const* pEntity, bool arg2);
+	void ProcessStaticCounter();
+	void ProcessFirst();
+	void Process();
+	static void operator delete(void* arg1);
 };
 
 VALIDATE_SIZE(CPedIntelligence, 0x294);
