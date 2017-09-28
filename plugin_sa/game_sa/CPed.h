90a3c0c1a4ba9d660b5fe6bae3befa4df948eee5
Crspy <crspo2020@outlook.com>
2017-09-28 22:26:01 +0200

    added MoveState enums

--- plugin_sa/game_sa/CPed.h
+++ plugin_sa/game_sa/CPed.h
@@ -29,7 +29,14 @@ enum ePedStats {
 };
 
 enum eMoveState {
-
+   PEDMOVE_NONE = 0,
+   PEDMOVE_STILL,
+   PEDMOVE_TURN_L,
+   PEDMOVE_TURN_R,
+   PEDMOVE_WALK,           
+   PEDMOVE_JOG,      
+   PEDMOVE_RUN,
+   PEDMOVE_SPRINT
 };
 
 #pragma pack(push, 4)
