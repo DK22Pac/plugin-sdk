604f29b9b1adb41520ac702a9b1706358cba185d
DK22Pac <gdamdk@gmail.com>
2017-10-09 21:42:47 +0300

    fix - generate project files when there are spaces in plugin-sdk path

--- tools/generate/CodeBlocks.bat
+++ tools/generate/CodeBlocks.bat
@@ -1,2 +1,2 @@
-%PLUGIN_SDK_DIR%\tools\premake\premake5 codeblocks --file=%PLUGIN_SDK_DIR%\tools\premake\premake5.lua
+"%PLUGIN_SDK_DIR%\tools\premake\premake5" codeblocks --file="%PLUGIN_SDK_DIR%\tools\premake\premake5.lua"
 pause
