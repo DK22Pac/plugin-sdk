792a667162fa49a028ef16531bea594604e7c250
Crspy <crspo2020@outlook.com>
2017-10-24 03:17:39 +0200

    Added CColourSet Class
    Added  CTimeCycle Class , Added CColourSet Class , CTimeCycleBox class and updated CTaskUtilityLineUpPedWithCar

--- plugin_sa/game_sa/CTaskUtilityLineUpPedWithCar.h
+++ plugin_sa/game_sa/CTaskUtilityLineUpPedWithCar.h
@@ -10,14 +10,21 @@
 #include "CTaskSimple.h"
 #include "CPed.h"
 
-class PLUGIN_API CTaskUtilityLineUpPedWithCar : public CTaskSimple {
-protected:
-    CTaskUtilityLineUpPedWithCar(plugin::dummy_func_t a) : CTaskSimple(a) {}
+class PLUGIN_API CTaskUtilityLineUpPedWithCar{
 public:
+
+	CVector* pCoords;
+	float field_4;
+	int field_8;
+	int field_c;
+	int time;
+	int field_14;
+	int pCTaskSimpleVtable;
+
 	CTaskUtilityLineUpPedWithCar(CVector const &Vector, int nTime, int arg3, int arg4 );   
 	CVector* GetLocalPositionToOpenCarDoor(int unused, CVehicle* pVehicle, float arg3, CAnimBlendAssociation* pAnimBlendAssoc);
     void ProcessPed(CPed* pPed, CVehicle* pVehicle, CAnimBlendAssociation* pAnimBlendAssoc);
     RwV3d* GetPositionToOpenCarDoor(int unused, CVehicle* pVehicle, float arg2, CAnimBlendAssociation* pAnimBlendAssoc);
 };
 
-//VALIDATE_SIZE(CTaskUtilityLineUpPedWithCar, 0x);
+VALIDATE_SIZE(CTaskUtilityLineUpPedWithCar, 28);
