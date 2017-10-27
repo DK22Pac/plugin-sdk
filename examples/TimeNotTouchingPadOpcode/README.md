ab1ba5bff556f2bbfefc98fba1e5b21668ca5852
DK22Pac <gdamdk@gmail.com>
2017-10-28 02:49:15 +0300

    Added Custom command (opcode) example for CLEO SDK (GTASA)

---
+++ examples/TimeNotTouchingPadOpcode/README.md
@@ -0,0 +1,22 @@
+## TimeNotTouchingPadOpcode
+
+An opcode for CLEO library, written with CLEO SDK.
+
+**Works for**: GTASA
+
+### SannyBuilder example
+
+```{$CLEO}
+{$OPCODE 1A00=2,%2d% = get_player %1d% time_not_touching_pad}
+
+thread 'TimeTch'
+
+while true
+    wait 0
+	1A00: 0@ = get_player 0 time_not_touching_pad
+    if
+        0@ > 2000
+    then
+        0AD1: show_formatted_text_highpriority "You didn't touch the pad for %d ms" time 100 0@
+    end
+end```
---
+++ examples/TimeNotTouchingPadOpcode/TimeNotTouchingPadOpcode.cpp
@@ -0,0 +1,16 @@
+#include "CLEO.h"
+#include "game_sa\CTimer.h"
+#include "game_sa\CPad.h"
+
+class TimeNotTouchingPadOpcode {
+public:
+    static OpcodeResult WINAPI OpcodeFunc(CScriptThread* thread) {
+        int playerId = CLEO_GetIntOpcodeParam(thread);
+        CLEO_SetIntOpcodeParam(thread, CTimer::m_snTimeInMilliseconds - CPad::GetPad(playerId)->LastTimeTouched);
+        return OR_CONTINUE;
+    }
+
+    TimeNotTouchingPadOpcode() {
+        CLEO_RegisterOpcode(0x1A00, OpcodeFunc); // 1A00=2,%2d% = get_player %1d% time_not_touching_pad
+    }
+} timeNotTouchingPadOpcode;
