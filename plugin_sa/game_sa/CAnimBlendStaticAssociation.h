86e308249f1c6fa4a8ff31b8ea8003b205f27a0f
Crspy <crspo2020@outlook.com>
2017-11-21 10:41:32 +0200

    Update CAnimBlendStaticAssociation.h

--- plugin_sa/game_sa/CAnimBlendStaticAssociation.h
+++ plugin_sa/game_sa/CAnimBlendStaticAssociation.h
@@ -12,18 +12,19 @@ Do not delete this comment block. Respect others' work!
 class CAnimBlendStaticAssociation
 {
 public:
-	char pad[6];
-	__int16 sAnimID;
-	__int16 sAnimGroup;
-	__int16 sFlags;
-	char pad2[4];
-	CAnimBlendHierarchy *pAnimHeirarchy;
+	unsigned short m_numBlendNodes;                   // +0x04
+	short m_animID;                           // +0x06
+	short m_animGroup;                        // +0x08
+	short m_flags;                            // +0x0A
+	int* m_pAnimBlendNodesSequenceArray;      // +0x0C 
+	CAnimBlendHierarchy* m_pAnimBlendHier;  // +0x10
 
 
 	//funcs
 	void AllocateSequenceArray(int size);
 	CAnimBlendStaticAssociation();
 	void Init(RpClump* pClump, CAnimBlendHierarchy* pAnimBlendHier);
+	virtual ~CAnimBlendStaticAssociation() = 0;
 };
 
 VALIDATE_SIZE(CAnimBlendStaticAssociation, 0x14);
