b5af81377e5d352b04e7b0e6e012948f7248d766
Crspy <crspo2020@outlook.com>
2017-12-21 15:51:52 +0200

    updating CPlayerPed class

--- plugin_III/game_III/CPlayerPed.h
+++ plugin_III/game_III/CPlayerPed.h
@@ -15,7 +15,39 @@ Do not delete this comment block. Respect others' work!
 
 class CPlayerPed : public CPed {
 public:
-    char field_53C[0xB4];
+
+	CWanted *m_pWanted;
+	int *m_pArrestingCop;   //  CCopPed *m_pArrestingCop;
+	float m_fMoveSpeed;
+	float m_fCurrentStamina;
+	float m_fMaxStamina;
+	float m_fStaminaProgress;
+	char m_bWeaponSlot;
+	char m_bSpeedTimerFlag;
+	char m_bShouldEvade;
+	char field_1367;
+	int m_nSpeedTimer;
+	int m_nShotDelay;
+	float field_1376;
+	char field_1380;
+	char field_1381;
+	char field_1382;
+	char field_1383;
+	CEntity *m_pEvadingFrom;
+	int m_nTargettableObjects[4];
+	char m_bAdrenalineActive;
+	char m_bHasLockOnTarget;
+	char field_1406;
+	char field_1407;
+	int m_bAdrenalineTime;
+	char m_bCanBeDamaged;
+	char field_1413;
+	char field_1414;
+	char field_1415;
+	CVector field_1416[6];
+	int field_1488[6];
+	float field_1512;
+	float field_1516;
 
     //funcs
     void AnnoyPlayerPed(bool arg0);
