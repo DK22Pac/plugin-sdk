0fe11db2313edaae280ec2c2af58c7cfd156bdc6
Crspy <crspo2020@outlook.com>
2018-02-16 14:01:50 +0200

    Added VC CGameLogic

---
+++ plugin_vc/game_vc/CGameLogic.cpp
@@ -0,0 +1,42 @@
+/*
+Plugin-SDK (Grand Theft Auto) source file
+Authors: GTA Community. See more here
+https://github.com/DK22Pac/plugin-sdk
+Do not delete this comment block. Respect others' work!
+*/
+#include "CGameLogic.h"
+
+// Converted from cdecl void CGameLogic::InitAtStartOfGame(void)	0x42C480	
+void CGameLogic::InitAtStartOfGame() {
+    plugin::Call<0x42C480>();
+}
+
+// Converted from cdecl void CGameLogic::Update(void)	0x42BB70	
+void CGameLogic::Update() {
+    plugin::Call<0x42BB70>();
+}
+
+// Converted from cdecl void CGameLogic::RestorePlayerStuffDuringResurrection(CPlayerPed *arg1,CVector arg2,float arg3)	0x42B900	
+void CGameLogic::RestorePlayerStuffDuringResurrection(CPlayerPed* arg1, CVector arg2, float arg3) {
+    plugin::Call<0x42B900, CPlayerPed*, CVector, float>(arg1, arg2, arg3);
+}
+
+// Converted from cdecl void CGameLogic::ClearShortCut(void)	0x42B730	
+void CGameLogic::ClearShortCut() {
+    plugin::Call<0x42B730>();
+}
+
+// Converted from cdecl void CGameLogic::UpdateShortCut(void)	0x42AE70	
+void CGameLogic::UpdateShortCut() {
+    plugin::Call<0x42AE70>();
+}
+
+// Converted from cdecl void CGameLogic::AfterDeathArrestSetUpShortCutTaxi(void)	0x42AC10	
+void CGameLogic::AfterDeathArrestSetUpShortCutTaxi() {
+    plugin::Call<0x42AC10>();
+}
+
+// Converted from cdecl void CGameLogic::RemoveShortCutDropOffPointForMission(void)	0x42ADE0	
+void CGameLogic::RemoveShortCutDropOffPointForMission() {
+    plugin::Call<0x42ADE0>();
+}
---
+++ plugin_vc/game_vc/CGameLogic.h
@@ -0,0 +1,22 @@
+/*
+Plugin-SDK (Grand Theft Auto) header file
+Authors: GTA Community. See more here
+https://github.com/DK22Pac/plugin-sdk
+Do not delete this comment block. Respect others' work!
+*/
+#pragma once
+
+#include "plbase/PluginBase_VC.h"
+#include "CPlayerPed.h"
+
+class CGameLogic {
+public:
+	static void InitAtStartOfGame();
+    static void Update();
+    static void RestorePlayerStuffDuringResurrection(CPlayerPed* arg1, CVector arg2, float arg3);
+    static void ClearShortCut();
+    static void UpdateShortCut();
+    static void AfterDeathArrestSetUpShortCutTaxi();
+    static void RemoveShortCutDropOffPointForMission();
+	
+};
