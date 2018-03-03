4ecae2482b138a76cc8516b755eaba9b52148e70
HollywoodJack <33234728+HollywoodJack@users.noreply.github.com>
2018-03-03 15:27:34 +0100

    enum eCopType added
    Based on CPopulation::AddPedInCar function. More details here: 
    https://pastebin.com/FAXSBZFh

--- plugin_vc/game_vc/eCopType.h
+++ plugin_vc/game_vc/eCopType.h
@@ -8,4 +8,10 @@ Do not delete this comment block. Respect others' work!
 
 enum eCopType : unsigned int
 {
+    COP_TYPE_COP = 0
+    COP_TYPE_FBI = 1
+    COP_TYPE_SWAT1 = 2
+    COP_TYPE_SWAT2 = 3
+    COP_TYPE_ARMY = 4
+    COP_TYPE_VICE = 5
 };
