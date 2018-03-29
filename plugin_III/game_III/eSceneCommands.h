49a8ddf9f7f4b2113cd89865189d41e7c2fce78c
Crspy <crspo2020@outlook.com>
2018-03-29 09:47:09 +0200

    III/VC eSceneCommands & eMoveState

---
+++ plugin_III/game_III/eSceneCommands.h
@@ -0,0 +1,36 @@
+/*
+Plugin-SDK (Grand Theft Auto 3) header file
+Authors: GTA Community. See more here
+https://github.com/DK22Pac/plugin-sdk
+Do not delete this comment block. Respect others' work!
+*/
+#pragma once
+
+enum eSceneCommands {
+    COMMAND_DO_NOTHING = 0,
+    COMMAND_NEW_ACTOR = 1,
+    COMMAND_MOVE_ACTOR = 2,
+    COMMAND_SELECT_ACTOR = 3,
+    COMMAND_DELETE_ACTOR = 4,
+    COMMAND_NEW_VEHICLE = 5,
+    COMMAND_MOVE_VEHICLE = 6,
+    COMMAND_SELECT_VEHICLE = 7,
+    COMMAND_DELETE_VEHICLE = 8,
+    COMMAND_GIVE_WEAPON = 9,
+    COMMAND_GOTO = 10,
+    COMMAND_GOTO_WAIT = 11,
+    COMMAND_GET_IN_CAR = 12,
+    COMMAND_GET_OUT_CAR = 13,
+    COMMAND_KILL = 14,
+    COMMAND_FLEE = 15,
+    COMMAND_WAIT = 16,
+    COMMAND_POSITION_CAMERA = 17,
+    COMMAND_SET_CAMERA_TARGET = 18,
+    COMMAND_SELECT_CAMERA_MODE = 19,
+    COMMAND_SAVE_MOVIE = 20,
+    COMMAND_LOAD_MOVIE = 21,
+    COMMAND_PLAY_MOVIE = 22,
+    COMMAND_END = 23,
+    COMMAND_ACTION = 24,
+    COMMAND_SELECTED = 25
+};
