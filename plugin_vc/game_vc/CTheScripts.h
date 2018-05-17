db8e1f2a7a6104a445dc37a90a3aff94872fb49d
ZeyadAhmed <zeyadahmed113@gmail.com>
2018-05-17 14:00:34 +0300

    [VC]CTheScripts - IsPlayerOnAMission(), IsPlayerStopped(), IsVehicleStopped()

--- plugin_vc/game_vc/CTheScripts.h
+++ plugin_vc/game_vc/CTheScripts.h
@@ -8,10 +8,16 @@
 
 #include "PluginBase.h"
 #include "CPlayerInfo.h"
-
+#include "CVehicle.h"
 extern unsigned int MAX_SCRIPT_SPACE_SIZE; // default 260512
 
 class PLUGIN_API CTheScripts {
 public:
     static unsigned char *ScriptSpace; // static unsigned char ScriptSpace[MAX_SCRIPT_SPACE_SIZE]
+
+	static bool CTheScripts::IsPlayerOnAMission();
+
+	static bool CTheScripts::IsPlayerStopped(CPlayerInfo* player);
+
+	static bool CTheScripts::IsVehicleStopped(CVehicle* vehicle);
 };
