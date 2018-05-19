3d4be8e823d0e3554d81aee24c17605b33fae729
ZeyadAhmed <zeyadahmed113@gmail.com>
2018-05-19 04:04:23 +0200

    [VC] Completing most of CTheScripts

--- plugin_vc/game_vc/CTheScripts.cpp
+++ plugin_vc/game_vc/CTheScripts.cpp
@@ -10,14 +10,44 @@ unsigned int MAX_SCRIPT_SPACE_SIZE = 260512;
 
 unsigned char *CTheScripts::ScriptSpace = (unsigned char *)0x821280;
 
+int &CTheScripts::NumberOfIntroTextLinesThisFrame = *(int*)0xA10A48;
+
+CTheScripts::tIntroText *CTheScripts::IntroTextLines = (CTheScripts::tIntroText*)0x7F0EA0;
+
+
 bool CTheScripts::IsPlayerOnAMission() {
 	return plugin::CallAndReturn<bool, 0x44FE30>();
 }
 
-bool CTheScripts::IsPlayerStopped(CPlayerInfo* player) {
-	return plugin::CallAndReturn<bool, 0x45ED50, CPlayerInfo*>(player);
+bool CTheScripts::IsPlayerStopped(CPlayerInfo* playerinfo) {
+	return plugin::CallAndReturn<bool, 0x45ED50, CPlayerInfo*>(playerinfo);
 }
 
 bool CTheScripts::IsVehicleStopped(CVehicle* vehicle) {
 	return plugin::CallAndReturn<bool, 0x45ED20, CVehicle*>(vehicle);
+}
+
+void CTheScripts::Process() {
+	plugin::Call<0x44FED0>();
+}
+
+signed int CTheScripts::Init() {
+	
+	return plugin::CallAndReturn<signed int, 0x450330>();
+}
+
+void CTheScripts::CleanUpThisPed(CPed* ped) {
+	plugin::Call<0x45EB10, CPed*>(ped);
+}
+
+void CTheScripts::CleanUpThisVehicle(CVehicle* vehicle) {
+	plugin::Call<0x45EAD0, CVehicle*>(vehicle);
+}
+
+void CTheScripts::CleanUpThisObject(CObject* object) {
+	plugin::Call<0x45EB80, CObject*>(object);
+}
+
+void CTheScripts::RemoveThisPed(CPed* ped) {
+	plugin::Call<0x45EC70, CPed*>(ped);
 }
--- plugin_vc/game_vc/CTheScripts.h
+++ plugin_vc/game_vc/CTheScripts.h
@@ -9,15 +9,52 @@
 #include "PluginBase.h"
 #include "CPlayerInfo.h"
 #include "CVehicle.h"
+#include "CRGBA.h"
+#include "CObject.h"
+
 extern unsigned int MAX_SCRIPT_SPACE_SIZE; // default 260512
+
+
+
+
 
 class PLUGIN_API CTheScripts {
 public:
     static unsigned char *ScriptSpace; // static unsigned char ScriptSpace[MAX_SCRIPT_SPACE_SIZE]
+	static int &NumberOfIntroTextLinesThisFrame;
+
+	struct tIntroText {
+		float fWidth;
+		float fHight;
+		CRGBA color;
+		bool bCenter;
+		bool bBackground;
+		bool bBackgroundOnlyText;
+		float fSize, fCenterSize;
+		CRGBA backgroundColor;
+		bool bProportional;
+		bool bDisable;
+		bool bRightJustify;
+		int fontStyle;
+		float x;
+		float y;
+		wchar_t text[100];
+
+		int __pad;
+	};
 
 	static bool CTheScripts::IsPlayerOnAMission();
-
-	static bool CTheScripts::IsPlayerStopped(CPlayerInfo* player);
-
+	static bool CTheScripts::IsPlayerStopped(CPlayerInfo* playerinfo);
 	static bool CTheScripts::IsVehicleStopped(CVehicle* vehicle);
+	static void CTheScripts::Process();
+	static signed int CTheScripts::Init();
+	static void CTheScripts::CleanUpThisPed(CPed* ped);
+	static void CTheScripts::CleanUpThisVehicle(CVehicle* vehicle);
+	static void CTheScripts::CleanUpThisObject(CObject* object);
+	static void CTheScripts::RemoveThisPed(CPed* ped);
+	
+	static tIntroText *IntroTextLines;
+
+
+	
 };
