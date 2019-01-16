27e05f4db2e9899994a3a19b75c196598d3eb20c
saml1er <danishroar@gmail.com>
2019-01-16 05:01:34 +0500

    Add RpAnimBlend* functions for III and VC

--- plugin_vc/game_vc/common.h
+++ plugin_vc/game_vc/common.h
@@ -11,8 +11,10 @@
 #include "CPed.h"
 #include "CVehicle.h"
 #include "CTrain.h"
+#include "CAnimBlendClumpData.h"
 
 class CAnimBlendAssociation;
+class CAnimBlendHierarchy;
 
 float FindPlayerHeading();
 CVector& FindPlayerCentreOfWorld_NoSniperShift();
@@ -46,4 +48,38 @@ void SetLightsWithTimeOfDayColour(RpWorld* world);
 
 RpAtomic* GetCurrentAtomicObjectCB(RwObject* object, void* data);
 
+CAnimBlendClumpData* RpAnimBlendAllocateData(RpClump* clump);
+CAnimBlendAssociation* RpAnimBlendClumpAddAssociation(RpClump* clump, CAnimBlendAssociation* association, unsigned int flags, float startTime, float blendAmount);
+CAnimBlendAssociation* RpAnimBlendClumpExtractAssociations(RpClump* clump);
+void RpAnimBlendClumpFillFrameArray(RpClump* clump, AnimBlendFrameData** frameData);
+AnimBlendFrameData* RpAnimBlendClumpFindBone(RpClump* clump, unsigned int id);
+AnimBlendFrameData* RpAnimBlendClumpFindFrame(RpClump* clump, char const* name);
+AnimBlendFrameData* RpAnimBlendClumpFindFrameFromHashKey(RpClump* clump, unsigned int key);
+CAnimBlendAssociation* RpAnimBlendClumpGetAssociation(RpClump* clump, bool arg1, CAnimBlendHierarchy* hierarchy);
+CAnimBlendAssociation* RpAnimBlendClumpGetAssociation(RpClump* clump, char const* name);
 CAnimBlendAssociation* RpAnimBlendClumpGetAssociation(RpClump* clump, unsigned int animId);
+CAnimBlendAssociation* RpAnimBlendClumpGetFirstAssociation(RpClump* clump);
+CAnimBlendAssociation* RpAnimBlendClumpGetFirstAssociation(RpClump* clump, unsigned int flags);
+CAnimBlendAssociation* RpAnimBlendClumpGetMainAssociation(RpClump* clump, CAnimBlendAssociation** pAssociation, float* blendAmount);
+CAnimBlendAssociation* RpAnimBlendClumpGetMainAssociation_N(RpClump* clump, int n);
+CAnimBlendAssociation* RpAnimBlendClumpGetMainPartialAssociation(RpClump* clump);
+CAnimBlendAssociation* RpAnimBlendClumpGetMainPartialAssociation_N(RpClump* clump, int n);
+unsigned int RpAnimBlendClumpGetNumAssociations(RpClump* clump);
+unsigned int RpAnimBlendClumpGetNumNonPartialAssociations(RpClump* clump);
+unsigned int RpAnimBlendClumpGetNumPartialAssociations(RpClump* clump);
+void RpAnimBlendClumpGiveAssociations(RpClump* clump, CAnimBlendAssociation* association);
+void RpAnimBlendClumpInit(RpClump* clump);
+bool RpAnimBlendClumpIsInitialized(RpClump* clump);
+void RpAnimBlendClumpPauseAllAnimations(RpClump* clump);
+void RpAnimBlendClumpRemoveAllAssociations(RpClump* clump);
+void RpAnimBlendClumpRemoveAssociations(RpClump* clump, unsigned int flags);
+void RpAnimBlendClumpSetBlendDeltas(RpClump* clump, unsigned int flags, float delta);
+void RpAnimBlendClumpUnPauseAllAnimations(RpClump* clump);
+void RpAnimBlendClumpUpdateAnimations(RpClump* clump, float step, bool onScreen);
+RtAnimAnimation* RpAnimBlendCreateAnimationForHierarchy(RpHAnimHierarchy* hierarchy);
+char* RpAnimBlendFrameGetName(RwFrame* frame);
+void RpAnimBlendFrameSetName(RwFrame* frame, char* name);
+CAnimBlendAssociation* RpAnimBlendGetNextAssociation(CAnimBlendAssociation* association);
+CAnimBlendAssociation* RpAnimBlendGetNextAssociation(CAnimBlendAssociation* association, unsigned int flags);
+void RpAnimBlendKeyFrameInterpolate(void* voidOut, void* voidIn1, void* voidIn2, float time, void* customData);
+bool RpAnimBlendPluginAttach();
