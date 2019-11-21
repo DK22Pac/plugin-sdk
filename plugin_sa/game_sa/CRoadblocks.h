dcaf1bbd9714788f6c71f2eef77ee57278bdc080
HollywoodJack <33234728+HollywoodJack@users.noreply.github.com>
2019-11-21 16:35:07 +0100

    CRoadBlocks added

---
+++ plugin_sa/game_sa/CRoadblocks.h
@@ -0,0 +1,42 @@
+/*
+    Plugin-SDK (Grand Theft Auto San Andreas) header file
+    Authors: GTA Community. See more here
+    https://github.com/DK22Pac/plugin-sdk
+    Do not delete this comment block. Respect others work!
+*/
+#pragma once
+
+#include "PluginBase.h"
+#include "CVehicle.h"
+#include "CVector.h"
+
+struct tScriptRoadBlocks {
+	CVector	cornerA;
+	CVector	cornerB;
+	char	m_nActive;
+	char	m_bIsCreated;
+	char	type;
+	char	field_1B;
+};
+
+struct tRoadbloxDat {
+	short areaId;
+	short nodeId;
+};
+
+class PLUGIN_API CRoadBlocks {
+public:
+	SUPPORTED_10US static tScriptRoadBlocks(&aScriptRoadBlocks)[16]; // static tScriptRoadBlocks aScriptRoadBlocks[16]
+	SUPPORTED_10US static tRoadbloxDat(&roadbloxDat)[325]; // static tRoadbloxDat roadbloxDat[325]
+	SUPPORTED_10US static unsigned int &roadbloxFlags;
+	SUPPORTED_10US static unsigned int &roadbloxDatSize;
+
+	SUPPORTED_10US_11US static void CRoadBlocks::RegisterScriptRoadBlock(CVector *cornerA, CVector *cornerB, byte type);
+	SUPPORTED_10US_11US static void CRoadBlocks::ClearScriptRoadBlocks();
+	SUPPORTED_10US static void CRoadBlocks::Init();
+	SUPPORTED_10US_11US static void CRoadBlocks::GenerateRoadBlockCopsForCar(CVehicle *car, int pedsPositionsType, byte type);
+	SUPPORTED_10US_11US static void CRoadBlocks::CreateRoadBlockBetween2Points(CVector *a, CVector *b, byte type);
+	SUPPORTED_10US_11US static void CRoadBlocks::GenerateRoadBlocks();
+};
+
+#include "meta/meta.CRoadBlocks.h"
