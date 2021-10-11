0cc7e7d3d90d7cf7246a5a6dbd39391c447d9b9d
_AG <gennariarmando@outlook.com>
2021-10-11 05:22:00 +0200

    [SA] Fix CZone

--- plugin_sa/game_sa/CZoneInfo.h
+++ plugin_sa/game_sa/CZoneInfo.h
@@ -1,21 +1,21 @@
 /*
-Plugin-SDK (Grand Theft Auto San Andreas) header file
-Authors: GTA Community. See more here
-https://github.com/DK22Pac/plugin-sdk
-Do not delete this comment block. Respect others' work!
+	Plugin-SDK (Grand Theft Auto San Andreas) header file
+	Authors: GTA Community. See more here
+	https://github.com/DK22Pac/plugin-sdk
+	Do not delete this comment block. Respect others' work!
 */
 #pragma once
 #include "PluginBase.h"
-#include "RenderWare.h"
+#include "CRGBA.h"
 
 class PLUGIN_API CZoneInfo
 {
 public:
-	char GangDensity[10];
-	char DrugDealerCounter;
-	RwRGBA ZoneColor;
-	char Flags1;
-	char Flags2;
+	char m_nGangDensity[10];
+	char m_nDealerCounter;
+	CRGBA m_ZoneColor;
+	char m_nFlags;
+	char m_nPopulationRace;
 };
 
 VALIDATE_SIZE(CZoneInfo, 0x11);
