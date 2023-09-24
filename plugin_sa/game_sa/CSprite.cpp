16b96afe65791706176b4cffeb4a8a76498e4d59
TheArtemMaps <91487356+TheArtemMaps@users.noreply.github.com>
2023-09-24 21:32:37 +0300

    New sprite functions for SA
    Added new sprite functions for GTA SA

--- plugin_sa/game_sa/CSprite.cpp
+++ plugin_sa/game_sa/CSprite.cpp
@@ -16,6 +16,23 @@ void CSprite::RenderOneXLUSprite(float x, float y, float z, float halfWidth, flo
 		red, green, blue, alpha, rhw, intensity, udir, vdir);
 }
 
+void CSprite::InitSpriteBuffer() {
+	plugin::Call<0x70CFB0>();
+}
+
+void CSprite::FlushSpriteBuffer() {
+	plugin::Call<0x70CF20>();
+}
+
+void CSprite::RenderBufferedOneXLUSprite_Rotate_Aspect(float x, float y, float z, float w, float h, uint8_t r, uint8_t g, uint8_t b, int16_t intens, float recipz, float rotation, uint8_t a)
+{
+	plugin::Call<0x70E780, float, float, float, float, float, unsigned char, unsigned char, unsigned char, short, float, unsigned char>(x, y, z, w, h, r, g, b, intens, recipz, rotation, a);
+}
+
+void CSprite::RenderBufferedOneXLUSprite_Rotate_2Colours(float x, float y, float z, float w, float h, uint8_t r1, uint8_t g1, uint8_t b1, uint8_t r2, uint8_t g2, uint8_t b2, float cx, float cy, float recipz, float rotation, uint8_t a) {
+	plugin::Call<0x70EDE0, float, float, float, float, float, unsigned char, unsigned char, unsigned char, short, float, unsigned char>(x, y, z, w, h, r1, g1, b1, r2, g2, b2, cx, cy, recipz, rotation, a);
+}
+
 bool CSprite::CalcScreenCoors(RwV3d const &posn, RwV3d *out, float *w, float *h, bool checkMaxVisible, bool checkMinVisible)
 {
 	return ((bool (__cdecl *)(RwV3d const &, RwV3d *, float *, float *, bool, bool))0x70CE30)
--- plugin_sa/game_sa/CSprite.h
+++ plugin_sa/game_sa/CSprite.h
@@ -15,5 +15,9 @@ public:
 	static void RenderOneXLUSprite(float x, float y, float z, float halfWidth, float halfHeight, 
 		unsigned char red, unsigned char green, unsigned char blue, short alpha, float rhw, 
 		unsigned char intensity, unsigned char udir, unsigned char vdir);
+	static void FlushSpriteBuffer(void);
+	static void InitSpriteBuffer(void);
+	static void RenderBufferedOneXLUSprite_Rotate_2Colours(float x, float y, float z, float w, float h, uint8_t r1, uint8_t g1, uint8_t b1, uint8_t r2, uint8_t g2, uint8_t b2, float cx, float cy, float recipz, float rotation, uint8_t a);
+	static void RenderBufferedOneXLUSprite_Rotate_Aspect(float x, float y, float z, float w, float h, uint8_t r, uint8_t g, uint8_t b, int16_t intens, float recipz, float roll, uint8_t a);
 	static bool CalcScreenCoors(RwV3d const &posn, RwV3d *out, float *w, float *h, bool checkMaxVisible, bool checkMinVisible);
 };
