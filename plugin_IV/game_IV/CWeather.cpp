abf4aa5ac9932174db33170127398ecfd49c67e0
_AG <gennariarmando@outlook.com>
2023-11-14 22:33:35 +0100

    Add CWeather::Wind

--- plugin_IV/game_IV/CWeather.cpp
+++ plugin_IV/game_IV/CWeather.cpp
@@ -12,4 +12,5 @@ int32_t& CWeather::ForcedWeatherType = *gpatternt(int32_t, "A3 ? ? ? ? F3 0F 10 
 float& CWeather::InterpolationValue = *gpatternt(float, "F3 0F 11 05 ? ? ? ? 8B 46 58", 4);
 int32_t& CWeather::WeatherTypeInList = *gpatternt(int32_t, "A3 ? ? ? ? F3 0F 10 46 ? F3 0F 11 05 ? ? ? ? 0F B6 46 60", 1);
 float& CWeather::Rain = *gpatternt(float, "F3 0F 11 05 ? ? ? ? 0F B6 46 60", 4);
+float& CWeather::Wind = *gpatternt(float, "C7 05 ? ? ? ? ? ? ? ? C7 05 ? ? ? ? ? ? ? ? C3 CC CC CC CC CC CC 80 7C 24", 2);
 
