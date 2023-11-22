a141ce69dcebfdacccc41efb5a1a9c4525525b53
_AG <gennariarmando@outlook.com>
2023-11-22 23:00:53 +0100

    Update CWanted

--- plugin_IV/game_IV/CWanted.h
+++ plugin_IV/game_IV/CWanted.h
@@ -7,6 +7,16 @@
 #pragma once
 #include "PluginBase.h"
 
+enum eWantedLevel : int32_t {
+    WANTED_CLEAN = 0x0,
+    WANTED_LEVEL1 = 0x1,
+    WANTED_LEVEL2 = 0x2,
+    WANTED_LEVEL3 = 0x3,
+    WANTED_LEVEL4 = 0x4,
+    WANTED_LEVEL5 = 0x5,
+    WANTED_LEVEL_LAST = 0x6,
+};
+
 class CWanted {
 public:
     uint8_t field_1[8];
@@ -19,21 +29,29 @@ public:
     uint8_t m_nShockingEvents;
     uint8_t field_73;
 
-    uint8_t m_bIgnoredByPolice : 1;
-    uint8_t m_nWantedFlags1_2 : 1;
-    uint8_t m_bIgnoredByEveryone : 1;
-    uint8_t m_bIgnoredByLowPriorityEvents : 1;
-    uint8_t m_nWantedFlags1_5 : 1;
-    uint8_t m_nWantedFlags1_6 : 1;
-    uint8_t m_nWantedFlags1_7 : 1;
-    uint8_t m_nWantedFlags1_8 : 1;
+    uint8_t m_PoliceBackOff : 1;
+    uint8_t m_PoliceBackOffGarage : 1;
+    uint8_t m_EverybodyBackOff : 1;
+    uint8_t m_AllRandomsFlee : 1;
+    uint8_t m_AllNeutralRandomsFlee : 1;
+    uint8_t m_AllRandomsOnlyAttackWithGuns : 1;
+    uint8_t m_IgnoreLowPriorityShockingEvents : 1;
+    uint8_t m_DontDispatchCopsForThisPlayer : 1;
 
-    uint8_t m_nFlags;
+    uint8_t m_MaintainFlashingStarAfterOffence : 1;
+    uint8_t m_CenterWantedPosition : 1;
+    uint8_t m_SuppressLosingWantedLevelIfHiddenThisFrame : 1;
+    uint8_t m_AllowEvasionHUDIfDisablingHiddenEvasionThisFrame : 1;
+    uint8_t m_HasLeftInitialSearchArea : 1;
+    uint8_t m_DisableCallPoliceThisFrame : 1;
+    uint8_t m_LawPedCanAttackNonWantedPlayer : 1;
+
     uint8_t field_76[14];
     uint32_t m_nFlashingAfterOffence;
     uint8_t field_574[574];
-    uint8_t field_750[214];
-    int32_t m_nWantedLevel;
+    uint8_t field_750[210];
+    eWantedLevel m_WantedLevelOld;
+    eWantedLevel m_WantedLevel;
 
 public:
     static void SetMaximumWantedLevel(int32_t level);
