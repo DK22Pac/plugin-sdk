7cdd05fd2dedc794598d84bae1c93faf5e45e7f0
TheArtemMaps <91487356+TheArtemMaps@users.noreply.github.com>
2024-01-14 20:29:30 +0200

    Comment block

--- plugin_vc/game_vc/CPlaneBanners.cpp
+++ plugin_vc/game_vc/CPlaneBanners.cpp
@@ -1,4 +1,9 @@
-
+/*
+    Plugin-SDK (Grand Theft Auto Vice City) source file
+    Authors: GTA Community. See more here
+    https://github.com/DK22Pac/plugin-sdk
+    Do not delete this comment block. Respect others' work!
+*/
 #include "CPlaneBanners.h"
 
 void CPlaneBanners::Init()
--- plugin_vc/game_vc/CPlaneTrails.cpp
+++ plugin_vc/game_vc/CPlaneTrails.cpp
@@ -1,3 +1,10 @@
+/*
+    Plugin-SDK (Grand Theft Auto Vice City) source file
+    Authors: GTA Community. See more here
+    https://github.com/DK22Pac/plugin-sdk
+    Do not delete this comment block. Respect others' work!
+*/
+
 #include "CPlaneTrails.h"
 
 
