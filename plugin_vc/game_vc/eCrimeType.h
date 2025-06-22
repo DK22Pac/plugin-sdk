9fd798c7199ce505d5671cf05d66553a3b97fa36
Miran <Miran4@op.pl>
2025-06-22 22:14:52 +0200

    Added eCrimeType enum values for III and VC.
    Unified value names across the trilogy.

--- plugin_vc/game_vc/eCrimeType.h
+++ plugin_vc/game_vc/eCrimeType.h
@@ -7,5 +7,25 @@ Do not delete this comment block. Respect others' work!
 #pragma once
 
 enum eCrimeType {
-
+    CRIME_NONE,
+    CRIME_WEAPON_FIRE,
+    CRIME_PED_HIT,
+    CRIME_COP_HIT,
+    CRIME_PED_SHOOT,
+    CRIME_COP_SHOOT,
+    CRIME_CAR_STEAL,
+    CRIME_RUN_REDLIGHT,
+    CRIME_HIT_AND_RUN,
+    CRIME_SPEEDING,
+    CRIME_PED_RUNOVER,
+    CRIME_COP_RUNOVER,
+    CRIME_HELI_DESTROY,
+    CRIME_PED_ARSON,
+    CRIME_COP_ARSON,
+    CRIME_CAR_ARSON,
+    CRIME_PLANE_DESTROY,
+    CRIME_EXPLOSION,
+    CRIME_PED_STAB,
+    CRIME_COP_STAB,
+    CRIME_MAX
 };
