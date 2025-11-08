363e38a621f14c3bfbbaf9b0899251ea3084ed99
Miran <Miran4@op.pl>
2025-11-08 15:45:28 +0100

    Introduced Unit Tests

---
+++ examples/UnitTests/source/Main.cpp
@@ -0,0 +1,39 @@
+#include <plugin.h>
+#include "utest.h"
+#include "Test_CVector.h"
+
+using namespace plugin;
+
+UTEST_STATE();
+
+struct Main
+{
+    Main()
+    {
+        // register event callbacks
+        Events::menuDrawingEvent += []{ gInstance.RunTests(); };
+    }
+
+    void RunTests()
+    {
+        // just once
+        static bool ran = false;
+        if (ran) return;
+        ran = true;
+
+        if(AllocConsole())
+        {
+            SetWindowText(GetConsoleWindow(), "Plugin-SDK Unit Tests");
+            //RemoveMenu(GetSystemMenu(GetConsoleWindow(), FALSE), SC_CLOSE, MF_BYCOMMAND);
+            
+            // redirect output into new console
+            FILE* fDummy;
+            freopen_s(&fDummy, "CONOUT$", "w", stdout);
+            std::cout.clear();
+            std::clog.clear();
+        }
+
+        utest_main(__argc, __argv);
+    }
+} gInstance;
+
