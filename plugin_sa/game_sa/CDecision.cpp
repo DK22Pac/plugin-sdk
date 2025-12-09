9d8e82053debbc80ce2c6eede8add211a4fcff37
Miran <Miran4@op.pl>
2025-12-10 00:05:32 +0100

    CDecision updates

---
+++ plugin_sa/game_sa/CDecision.cpp
@@ -0,0 +1,11 @@
+/*
+    Plugin-SDK (Grand Theft Auto San Andreas) source file
+    Authors: GTA Community. See more here
+    https://github.com/DK22Pac/plugin-sdk
+    Do not delete this comment block. Respect others' work!
+*/
+#include "CDecision.h"
+
+CDecision::CDecision() {
+    plugin::CallMethod<0x6040C0, CDecision*>(this);
+}
